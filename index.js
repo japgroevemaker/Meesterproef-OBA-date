a6b8f2c39c671787ec0d4ee4c59ee3fb8ac007a0
japgroevemaker <joepgravemaker@gmail.com>
2019-06-04 11:54:21 +0200

    setting up server

---
+++ index.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let ejs = require('ejs');
+let bodyParser = require('body-parser');
+let socket = require('socket.io')
+let PORT = 5555
+
+let app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+  res.render("index")
+})
+
+app.listen(PORT)
+console.log(`Port ${PORT}`);
+
+let io = socket(server);
+
+io.on('connection', function(socket){
+  console.log('user connected');
+})
