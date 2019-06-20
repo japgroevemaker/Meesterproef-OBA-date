b2b63fe78d567a343dc88b16937e9b78a1d4802f
japgroevemaker <joepgravemaker@gmail.com>
2019-06-20 11:33:20 +0200

    styling together path

--- public/javascript/prev-next.js
+++ public/javascript/prev-next.js
@@ -1,25 +1,24 @@
 import getInput from './catchInput.js'
-if(window.location.pathname == '/shareKnowledge'){
 
   let currentPage = 0;
-  
+
   let next = document.querySelector('.next');
   let prev = document.querySelector('.prev')
-  
+
   next.addEventListener('click', function(){
     getInput.getInput.locate()
     currentPage++
     console.log(currentPage);
-    
+
     let section = document.querySelectorAll('.section');
     section.forEach(function(hide){
       hide.classList.add('none')
     })
-  
+
     document.getElementById(currentPage).classList.remove('none')
     document.getElementById(currentPage).classList.add('section')
   })
-  
+
   prev.addEventListener('click', function(){
     currentPage--
     console.log(currentPage);
@@ -28,8 +27,6 @@ if(window.location.pathname == '/shareKnowledge'){
     section.forEach(function(hide){
       hide.classList.add('none')
     })
-  
+
     document.getElementById(currentPage).classList.remove('none')
   })
-
-}
