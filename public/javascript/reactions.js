867d3b2cc3c0d19ddf93796cd81b6a13b91fced3
japgroevemaker <joepgravemaker@gmail.com>
2019-06-24 13:57:36 +0200

    styled reaction

--- public/javascript/reactions.js
+++ public/javascript/reactions.js
@@ -16,14 +16,30 @@
       e.preventDefault()
       let reactionContainer = document.querySelector(`#${uniqueSectionID} #reactions`)
 
+      let time = Date()
+      let timeStamp = time.slice(16, 21)
+      console.log(timeStamp);
+
+      let date = Date()
+      let dateStamp = date.slice(4, 10)
+      console.log(dateStamp);
+
+      let reactionImg = document.createElement('img');
+      reactionImg.setAttribute('src', '/assets/placeholder.jpg')
       let reactionBox = document.createElement('div');
-      reactionBox.setAttribute('class', 'reaction')
+      reactionBox.setAttribute('class', 'reaction');
+      let dateTimeParagraph = document.createElement('p');
+      dateTimeParagraph.setAttribute('class', 'date-time')
+      dateTimeParagraph.innerHTML = `${dateStamp} ${timeStamp}`
       let reaction = document.createElement('p');
       console.log(e.target);
       reaction.innerHTML = f.childNodes[1].value
 
+      reactionContainer.appendChild(reactionImg)
       reactionBox.appendChild(reaction)
-      reactionContainer.appendChild(reactionBox)
+      reactionContainer.appendChild(reactionBox);
+      reactionContainer.appendChild(dateTimeParagraph);
 
+      f.childNodes[1].value = ""
     })
   })
