412521864ecc490ac483212cc4f19eceea23784c
japgroevemaker <joepgravemaker@gmail.com>
2019-07-02 18:14:42 +0200

    fixed small bugs

--- public/javascript/unfold-nav.js
+++ public/javascript/unfold-nav.js
@@ -1,17 +1,20 @@
 let trigger = document.querySelector('#add-post');
-
 let cross = document.querySelector('#add-post img')
-
-
-
 let nav = document.querySelector('nav');
-
+let dashboardSearch = document.querySelector('#dashboard-search')
 
 if(window.location.pathname === "/dashboard" || '/'){
 
 trigger.addEventListener('click', function(){
   nav.classList.toggle('nav-unfold');
   cross.classList.toggle('rotate')
+
+  if (nav.classList.contains('nav-unfold')) {
+    dashboardSearch.style = 'box-shadow: none !important;';
+    console.log('contains');
+  } else {
+    header.style = 'box-shadow: 0px 0px 14px -4px rgba(0,0,0,0.24) !important;'
+  }
 });
 
 }
