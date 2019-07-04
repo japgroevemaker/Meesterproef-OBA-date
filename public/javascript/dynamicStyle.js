c1d2ed169192ec60ba7ec147d6113e0d40291154
Stan Ooms <35112551+Stanargy@users.noreply.github.com>
2019-07-04 18:21:04 +0200

    fixed reaction date error. also restructured the way reactions are stored in the database's post object

---
+++ public/javascript/dynamicStyle.js
@@ -0,0 +1,16 @@
+import colors from './dashboard.js'
+
+console.log(colors)
+
+const element = document.querySelector("body")
+
+
+if(window.location.pathname == '/helpme'){
+    element.style.background = colors[5]
+}
+if(window.location.pathname == '/somethingTogether'){
+    element.style.background = colors[4]
+}
+if(window.location.pathname == '/shareKnowledge'){
+    element.style.background = colors[3]
+}
